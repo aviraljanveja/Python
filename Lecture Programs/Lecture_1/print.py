a947b5984a5abd3c2ae3c8cb4fd80bbb0d93e573
Aviral Janveja <aviraljanveja@gmail.com>
2023-12-11 11:58:47 +0530

    directory structure change plus lecture programs added

---
+++ Lecture Programs/Lecture_1/print.py
@@ -0,0 +1,14 @@
+# Here is the basic print command.
+
+print("Hello World!")
+# Output : Hello World!
+
+# It is particularly useful for quickly checking
+# the values of variables or printing messages to understand
+# how your code is executing.
+
+# You can also concatenate strings within the print statement
+# using the + operator.
+
+print("Hello" + " " + "World." + " " + "My name is Python.")
+# Output : Hello World. My name is Python.
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Python and Algorithms
+These are my notes of the MIT course, 6.0001 : Introduction to Computer Science and Programming in Python. The book that goes by the same name, by Prof. John Guttag, has been used as the essential reading material.
 
-- Python Programming
-- Libraries & Frameworks
-- Algorithms
-- Data Structures
+What is Included ? 
+- Lectures
+- Lecture Programs
+- Solved problem sets
