6087450dfb57fe44f2ea242b8da699373ea5f9bf
Aviral Janveja <aviraljanveja@gmail.com>
2023-12-12 18:59:23 +0530

    add variable.py file

---
+++ Lecture Programs/Lecture_1/variable.py
@@ -0,0 +1,19 @@
+# Variable assignment
+name = "Avi"
+
+# Using variables
+print("My name is " + name + ".")  # Output: My name is Avi.
+
+# Examples of different data types
+integer_variable = 42
+float_variable = 3.14
+string_variable = "Hello, Python!"
+boolean_variable = True
+my_variable = None
+
+# Using the built-in type() function
+print(type(integer_variable))  # Output: <class 'int'>
+print(type(float_variable))  # Output: <class 'float'>
+print(type(string_variable))  # Output: <class 'str'>
+print(type(boolean_variable))  # Output: <class 'bool'>
+print(type(my_variable))  # Output: <class 'NoneType'>
