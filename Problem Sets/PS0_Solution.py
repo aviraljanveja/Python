9477cc814f4a344265eee05307cab27c545cdcc9
Aviral Janveja <aviraljanveja@gmail.com>
2023-12-21 00:10:09 +0530

    add MIT problem set 0 solution

--- Problem Sets/PS0_Solution.py
+++ Problem Sets/PS0_Solution.py
@@ -1,7 +1,20 @@
-import numpy
+# Assignment:
+# Write a program that does the following in order:
+# 1. Asks the user to enter a number “x”
+# 2. Asks the user to enter a number “y”
+# 3. Prints out number “x”, raised to the power “y”.
+# 4. Prints out the log (base 2) of “x”.
 
-x = int(input("Enter a number : "))
-y = int(input("Enter another number : "))
+
+# Solution:
+import numpy  # Importing numpy package
+
+# Taking numerical input from the user.
+x = float(input("Enter a number : "))
+y = float(input("Enter another number : "))
+
+# Using exponent operator to calculate x raised to the power y.
 print(x, "raised to the power", y, "is equal to :", x ** y)
-print("log(base2) of", x, "is equal to :", numpy.log2(x))
 
+# Using numpy package for calculating logarithm of the variable.
+print("log(base2) of", x, "is equal to :", numpy.log2(x))
