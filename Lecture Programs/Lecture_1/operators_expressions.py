1ccdb70f8896d7269c71dfce284aa4283e02e9b7
Aviral Janveja <aviraljanveja@gmail.com>
2023-12-20 17:54:25 +0530

    add operators program

---
+++ Lecture Programs/Lecture_1/operators_expressions.py
@@ -0,0 +1,52 @@
+# Here is a summary of operators in Python.
+
+
+# Arithmetic Operators: Arithmetic operators are used to perform mathematical operations.
+result_addition = 5 + 3  # Addition
+print(result_addition)  # Output: 8
+
+result_subtraction = 10 - 7  # Subtraction
+print(result_subtraction)  # Output: 3
+
+result_multiplication = 4 * 6  # Multiplication
+print(result_multiplication)  # Output: 24
+
+result_division = 15 / 3  # Division
+print(result_division)  # Output: 5.0
+
+result_modulus = 17 % 3  # Modulus (remainder)
+print(result_modulus)  # Output: 2
+
+result_exponentiation = 2 ** 3  # Exponentiation
+print(result_exponentiation)  # Output: 8
+
+
+# Logical operators : Logical operators are used to combine or negate Boolean values.
+logical_and = True and False  # Logical AND
+print(logical_and)  # Output: False
+
+logical_or = True or False  # Logical OR
+print(logical_or)  # Output: True
+
+logical_not = not True  # Logical NOT
+print(logical_not)  # Output: False
+
+
+# Comparison Operators: Comparison operators are used to compare values and return a Boolean result.
+is_equal = (10 == 5)  # Equal to
+print(is_equal)  # Output: False
+
+is_not_equal = (10 != 5)  # Not equal to
+print(is_not_equal)  # Output: True
+
+is_greater_than = (10 > 5)  # Greater than
+print(is_greater_than)  # Output: True
+
+is_less_than = (11 < 10)  # Less than
+print(is_less_than)  # Output: False
+
+is_greater_equal = (10 >= 10)  # Greater than or equal to
+print(is_greater_equal)  # Output: True
+
+is_less_equal = (7 <= 10)  # Less than or equal to
+print(is_less_equal)  # Output: True
