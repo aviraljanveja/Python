933dbfee387c6b344423ffbf4cfe6de359c5990b
Aviral Janveja <aviraljanveja@gmail.com>
2024-01-08 20:29:02 +0530

    add user input program

---
+++ Lecture Programs/Lecture_1/user_input.py
@@ -0,0 +1,9 @@
+# Taking User Input.
+
+text = input("Type anything : ")
+# By default, python always treats user inputs as strings.
+# You need to cast to the appropriate data type to handle them.
+print(5 * text)  # If user input : 5, Output : 55555
+
+num = int(input("Type any integer : "))  # Casting type to integer.
+print(5 * num)  # If user input : 5, Output : 25
