f9a8f66238af275a33c109ba525cb6eb92de0379
Aviral Janveja <aviraljanveja@gmail.com>
2024-01-15 21:28:44 +0530

    add for loop code

---
+++ Chapter Programs/Chapter_2/iteration_for.py
@@ -0,0 +1,11 @@
+# Simple for loop
+
+for number in range(6):
+    print(number)
+
+print("--------------")
+
+# Iterating over a string and printing each character
+word = "Python"
+for char in word:
+    print(char)
