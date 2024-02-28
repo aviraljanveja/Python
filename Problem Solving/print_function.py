1f69d58a2886e1d3a29b43ee640f2a1563e0384c
Aviral Janveja <aviraljanveja@gmail.com>
2024-02-28 16:36:19 +0530

    remove problem solving programs

--- Problem Solving/print_function.py
+++
@@ -1,9 +0,0 @@
-# See the following link for problem statement:
-# https://www.hackerrank.com/challenges/python-print/problem
-
-n = int(input())
-if 1 <= n <= 150:
-    for i in range(1, n+1):
-        print(i, end='')
-else:
-    print("Enter an integer between 1 and 150!")
