260aa627e2bbbf3c57c799f69ec7be2184e0df18
Aviral Janveja <aviraljanveja@gmail.com>
2024-04-26 19:52:34 +0530

    add chapter 6 programs

---
+++ Chapter Programs/Chapter_6/debug_print.py
@@ -0,0 +1,16 @@
+# Example os using print statement as a debugging tool.
+
+def sum_even(numbers):
+    total = 0
+    print("Initial total:", total)  # Check the initial value of total
+    for number in numbers:
+        print("Current number:", number)  # Display the current number
+        if number % 2 == 0:
+            total += number
+            print("Added to total:", number)  # Indicate if the number is added to total
+    print("Final total:", total)  # Check the final value of total
+    return total
+
+
+num = (1, 2, 3, 4, 5, 6)
+sum_even(num)
