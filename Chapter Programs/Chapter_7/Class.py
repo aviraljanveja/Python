4a8f3811535bfdb9d382fa50d318c188ff9111e7
Aviral Janveja <aviraljanveja@gmail.com>
2024-09-07 23:51:38 +0530

    add ch 7 programs with program class.py

---
+++ Chapter Programs/Chapter_7/Class.py
@@ -0,0 +1,11 @@
+#  Defining our own class.
+
+
+class Coordinate:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+c = Coordinate(3, 4)  # Creating an object of type Coordinate
