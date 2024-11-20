7f33e25515150ff26d8ec2da7bd00ea303794e8f
Aviral Janveja <aviraljanveja@gmail.com>
2024-11-20 15:36:38 +0530

    add new programs to HackerRank python

---
+++ Problem Solving/HackerRank Select/Python - Loops.py
@@ -0,0 +1,7 @@
+# HackerRank Python : Loops
+# Problem Statement Link : https://www.hackerrank.com/challenges/python-loops/problem
+
+n = int(input())  # Read an integer input from the user
+
+for i in range(n):  # Loop through the range of numbers from 0 to n-1
+    print(i**2)  # Print the square of the current number i
