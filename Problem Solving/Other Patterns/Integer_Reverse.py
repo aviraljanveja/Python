3cb5288ed1024189b0e8eb12e63c37d46e3f37b1
Aviral Janveja <aviraljanveja@gmail.com>
2024-11-24 17:34:13 +0530

    add python integrer reverse program update

--- Problem Solving/Other Patterns/Integer_Reverse.py
+++ Problem Solving/Other Patterns/Integer_Reverse.py
@@ -8,13 +8,14 @@ result = 0
 
 # Start a loop that will continue as long as there are digits left in n
 while n > 0:
-    # Get the last digit of n using modulus operator
+    # Extract the last digit from n using modulus operator and store it in the remainder variable
     remainder = n % 10
 
-    # Add remainder to the current result, while moving current digits to the left via multiplication by 10
+    # Update the result by multiplying it with 10 and adding the last digit.
+    # This effectively appends the last digit to the end of the result.
     result = result * 10 + remainder
 
-    # Remove the last digit from n by doing integer division by 10
+    # Remove the last digit from n via integer division by 10
     n = n // 10
 
 # Print the reversed integer
