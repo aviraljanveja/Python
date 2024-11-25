08110013ea7187ff6a52b6a46223a6b52f9000c9
Aviral Janveja <aviraljanveja@gmail.com>
2024-11-25 16:50:50 +0530

    Python problem solving folder update 25.11.2024

---
+++ Problem Solving/HackerRank Python/README.md
@@ -0,0 +1,1 @@
+*Select problems from HackerRank Python domain*
--- Problem Solving/LeetCode/1 - Two Sum.py
+++ Problem Solving/LeetCode/1 - Two Sum.py
@@ -1,9 +1,13 @@
-# LeetCode : Two Sum
+# LeetCode 1 - Two Sum
 
 def twosum(arr, target):
+    # Outer loop to pick the first number
     for i in range(len(arr)):
+        # Inner loop to pick the second number, starting after the first
         for j in range(i+1, len(arr)):
+            # Check if the two numbers sum up to the target
             if arr[i] + arr[j] == target:
+                # If condition is met, return their indices
                 return [i,j]
 
 
