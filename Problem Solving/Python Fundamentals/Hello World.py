38a946b4c8a49d38c9aa2445fc51234b3df8ef50
Aviral Janveja <aviraljanveja@gmail.com>
2024-12-25 21:10:10 +0530

    add python fundamental question

--- Problem Solving/Python Fundamentals/Hello World.py
+++
@@ -1,7 +0,0 @@
-# HackerRank Python : Print Function
-# Problem Statement Link : https://www.hackerrank.com/challenges/python-print/problem
-
-n = int(input())  # Read an integer input n
-
-for i in range(1, n+1):  # Loop through numbers from 1 to n (n included)
-    print(i, end="")  # end="" ensures the print output remains on the same line
