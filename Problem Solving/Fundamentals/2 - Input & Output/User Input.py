e73eee31eb02ca6d4ddb9e76ec6451bdba4feef5
Aviral Janveja <aviraljanveja@gmail.com>
2024-12-27 16:56:13 +0530

    add user input program to input-output

---
+++ Problem Solving/Fundamentals/2 - Input & Output/User Input.py
@@ -0,0 +1,14 @@
+# User Input
+# The input() function is used to take user input as a string in Python.
+
+# Basic Input
+name = input("Enter your name : ")
+print(f"Hello {name} !")
+
+# Converting to different types.
+# The input is always read as a string, so you need to convert it to other types as needed.
+age = int(input("Enter your age : "))
+print(f"Wow, glad to know you are {age} years old !")
+
+pi = float(input("Enter the value of pi upto 5 decimal places : "))
+print(f"The pi is approximately : {pi:.2f}")
