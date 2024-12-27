5b6e03b5e49a9aba290c8c82c20389e8532adaa0
Aviral Janveja <aviraljanveja@gmail.com>
2024-12-27 21:16:02 +0530

    add conditional folder programs

---
+++ Problem Solving/Fundamentals/4 - Conditionals/Chaining conditions.py
@@ -0,0 +1,6 @@
+# Chaining conditions
+# Python supports chaining conditions for readability.
+
+x = 10
+if 0 < x < 20:
+    print("x is between 0 and 20")
---
+++ Problem Solving/Fundamentals/4 - Conditionals/if-membership check.py
@@ -0,0 +1,8 @@
+# Using if with in and not in for membership checks
+
+fruit = "pineapple"
+if "apple" in fruit:
+    print("apple in pineapple")
+
+if "grape" not in fruit:
+    print("grape not in pineapple")
