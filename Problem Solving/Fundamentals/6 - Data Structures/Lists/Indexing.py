bb510f7d3b983bb93e56029740f861c688543db3
Aviral Janveja <aviraljanveja@gmail.com>
2025-01-04 16:48:11 +0530

    add list fundamentals

---
+++ Problem Solving/Fundamentals/6 - Data Structures/Lists/Indexing.py
@@ -0,0 +1,10 @@
+# list : A list in Python is a built-in data structure that allows you
+# to store an ordered, mutable collection of elements.
+# Lists are extremely flexible and versatile compared to arrays in languages like Java or C++.
+
+# Indexing
+
+my_list = [1,2,3,4,5]
+
+print(my_list[0])  # Output = 1 (first element)
+print(my_list[-1])  # Output = 5 (last element)
