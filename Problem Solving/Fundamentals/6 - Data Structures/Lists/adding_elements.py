93254055c0321bdf28534f411184d2f811b3c46a
Aviral Janveja <aviraljanveja@gmail.com>
2025-01-04 17:15:18 +0530

    add list fundamentals 2

---
+++ Problem Solving/Fundamentals/6 - Data Structures/Lists/adding_elements.py
@@ -0,0 +1,15 @@
+# Adding Elements
+
+my_list = [1,2,3,4,5]
+
+# append() : Adds a single element to the end of the list.
+my_list.append(6)
+print(my_list)  # Output = [1, 2, 3, 4, 5, 6]
+
+# extend() : Adds multiple elements from another sequence to the end of the list.
+my_list.extend([7, 8, 9])
+print(my_list)  # Output = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# insert() : Adds a single element at a specific index, The existing elements are shifted to the right.
+my_list.insert(0, 0)
+print(my_list)  # Output = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
