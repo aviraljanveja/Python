90c10f2be8205427afc247a80ae2bb5c26518092
Aviral Janveja <aviraljanveja@gmail.com>
2025-01-14 19:10:08 +0530

    update LeetCode26 2 pointer solution to while loop implementation

--- Problem Solving/LeetCode/Array/Two Pointer/LeetCode 26 - Remove Duplicates from Sorted Array.py
+++ Problem Solving/LeetCode/Array/Two Pointer/LeetCode 26 - Remove Duplicates from Sorted Array.py
@@ -2,18 +2,21 @@
 # (https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/)
 
 def removeDuplicates(nums):
-    k = 1  # Pointer 1 = k, which represents the index to write the next unique element.
+    i = 1 # pointer 1, to Iterate through the sorted array.
+    k = 1  # Pointer 2, which represents the indices to write the unique elements.
 
-    for i in range(1, len(nums)):  # Pointer 2 = i to Iterate through the sorted array.
+    while i < len(nums):
+        # Comparing adjacent elements, utilizing the fact the array is sorted already.
         if nums[i] != nums[i-1]:  # Check if the current element is different from the previous one.
-            nums[k] = nums[i]   # If yes, then a new element is found. Write it to the current 'k' position.
-            k += 1  # Increment k, where we will write the next unique element.
+            nums[k] = nums[i]  # If yes, then a new element is found. Write it to the 'k' position.
+            k += 1  # Increment k, for storing the next unique element.
+        i += 1
 
-    return k  # Return k, which is the number of unique elements in the array.
+    return k  # After loop, Return k, which holds the number of unique elements in the array.
 
 
 # Test case
-arr1 = [0,0,1,1,1,2,2,3,3,4]
+arr1 = [0, 0, 0, 1, 1, 1, 2, 2, 2]
 solution1 = removeDuplicates(arr1)
-print(solution1)  # Output: 5 (unique elements count)
-print(arr1)  # Output: [0, 1, 2, 3, 4, 2, 2, 3, 3, 4]
+print(solution1)  # Output: 3 (number of unique elements = 0, 1, 2)
+print(arr1)  # Output array, duplicates removed in-place = [0, 1, 2, 3, 4, 2, 2, 3, 3, 4]
