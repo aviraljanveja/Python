804d6edfbb9fb392c3c31f274ca569bac182da86
Aviral Janveja <aviraljanveja@gmail.com>
2025-01-14 18:40:44 +0530

    minor update array largest code

--- Problem Solving/Fundamentals/Basic Problems/Array Largest.py
+++ Problem Solving/Fundamentals/Basic Problems/Array Largest.py
@@ -1,15 +1,15 @@
 # Largest Element in an Array
 
 def largestElement(nums):
-    max_num = float('-inf')  # Initialize max_num to negative infinity so it works for negative numbers too
+    largest = float('-inf')  # Initialize 'largest' to negative infinity so it works for negative numbers too
 
     for num in nums:  # Loop through each number in the list 'nums'
-        if num > max_num:
-            max_num = num   # If the current number is greater than max_num, update max_num
+        if num > largest:
+            largest = num   # If the current number is greater than largest, update largest.
 
-    return max_num  # Return the largest number found in the list
+    return largest  # Return the largest number found in the list
 
 
 # Test case
-nums1 = [-1, -2, -4, -7, -7, -5]
-print(largestElement(nums1))   # Output: -1
+nums1 = [-4, -2, 1, -7, -5]
+print(largestElement(nums1))   # Output: 1
