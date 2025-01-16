2bd91dc2213796358126ac1e134796869512d38a
Aviral Janveja <aviraljanveja@gmail.com>
2025-01-16 18:15:07 +0530

    modify leetcode 344

--- Problem Solving/LeetCode/String/Two Pointer/LeetCode 344 - Reverse String.py
+++ Problem Solving/LeetCode/String/Two Pointer/LeetCode 344 - Reverse String.py
@@ -2,10 +2,12 @@
 # Problem Link : https://leetcode.com/problems/reverse-string/description/
 
 def reverseString(s):
-    j = len(s) - 1  # Initialize the right pointer 'j' to the last index of the list
-    for i in range(len(s) // 2):  # Loop through the first half of the list using 'i' as the left pointer
-        s[i], s[j] = s[j], s[i]  # Swap the elements at indices 'i' and 'j'
-        j -= 1  # the right pointer 'j' moves one step towards the center
+    i = 0  # Pointer 1: Starts from the beginning of the list
+    j = len(s) - 1  # Pointer 2: Starts from the end of the list
+    while i < j:  # Swap elements until the pointers meet
+        s[i], s[j] = s[j], s[i]
+        i += 1  # Move Pointer 1 towards the right
+        j -= 1  # Move Pointer 2 towards the left
 
     return s  # The list is modified in place, so we can return it if needed
 
@@ -14,5 +16,5 @@ def reverseString(s):
 s1 = ["h","e","l","l","o"]
 s2 = ["p","y","t","h","o","n"]
 
-print(reverseString(s1))  # Output: ["o", "l", "l", "e", "h"]
-print(reverseString(s2))  # Output: ["n", "o", "h", "t", "y", "p"]
+print(reverseString(s1))  # Output = ['o', 'l', 'l', 'e', 'h']
+print(reverseString(s2))  # Output = ['n', 'o', 'h', 't', 'y', 'p']
