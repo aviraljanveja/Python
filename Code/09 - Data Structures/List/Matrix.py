e4304a1371f69a2e0984271eaef5c6d8f48de6db
Aviral Janveja <aviraljanveja@gmail.com>
2026-04-27 20:00:56 +0530

    python code ch 9 2d list update again!

--- Code/09 - Data Structures/List/Matrix.py
+++ Code/09 - Data Structures/List/Matrix.py
@@ -17,7 +17,7 @@ print(matrix[2][2])  # Output = 9 (Third row, third column)
 # Iteration by Value
 for row in matrix:  # First loop iterates over each row (which is a list).
     for value in row:  # Second loop iterates over each element in that row.
-        print(value, end=" ")
+        print(value, end = " ")
     print()  # Linebreak after each row
 
 # Output :
@@ -32,17 +32,16 @@ print() # Linebreak
 # Iteration by Index
 for i in range(len(matrix)):  # range(len(matrix)) gives row indices.
     for j in range(len(matrix[i])):  # range(len(matrix[i])) gives element indices for each row.
-        print(f"matrix[{i}][{j}] = {matrix[i][j]}")
+        print(f"matrix[{i}][{j}] = {matrix[i][j]}", end = " | ")
+    print()  # Linebreak after each row
+    print("-" * 56)  # Separator between rows
 
 """
 Output :
-matrix[0][0] = 1
-matrix[0][1] = 2
-matrix[0][2] = 3
-matrix[1][0] = 4
-matrix[1][1] = 5
-matrix[1][2] = 6
-matrix[2][0] = 7
-matrix[2][1] = 8
-matrix[2][2] = 9
+matrix[0][0] = 1 | matrix[0][1] = 2 | matrix[0][2] = 3 | 
+--------------------------------------------------------
+matrix[1][0] = 4 | matrix[1][1] = 5 | matrix[1][2] = 6 | 
+--------------------------------------------------------
+matrix[2][0] = 7 | matrix[2][1] = 8 | matrix[2][2] = 9 | 
+--------------------------------------------------------
 """
