17d5ee08c6d685a81ef20999d1426cfe50f8448b
Aviral Janveja <aviraljanveja@gmail.com>
2026-05-02 10:19:19 +0530

    Python code ch 5 functions update

--- Code/05 - Functions/function_definition.py
+++ Code/05 - Functions/function_definition.py
@@ -1,20 +1,18 @@
-# A function is a block of organized, reusable code that performs a specific task.
+# A function is a block of code that performs a specific task. 
+# They are reusuable and hence make your code more organized.
 
 
-# Function definition
-# Functions are defined using the def keyword, followed by the function name
+# Functions are defined using the def keyword, followed by the function name and parentheses().
 # Parameters are the variables listed within the parentheses of the function definition,
-# acting as placeholders for the values that will be passed in, for example "n" here.
+# acting as placeholders for the actual values that will be passed in while calling the function, for example "n" here.
 def isEven(n):
     if n % 2 == 0:
-        return True
+        return True  # The return statement is used to exit a function and return a value to the caller. In this case, it returns True if n is even.
     else:
-        return False
-        # Functions can optionally return a value using the return statement.
-        # If no return statement is present, the function implicitly returns None.
+        return False  # If no return statement is present, the function implicitly returns None.
 
 
-# Calling a function
+# Calling a function : function_name(arguments)
 # Arguments are the actual values that are passed to the function when it is called, for example 3 and 4 here.
 print(isEven(3))  # Output = False
 print(isEven(4))  # Output = True
