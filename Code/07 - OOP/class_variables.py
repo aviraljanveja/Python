fb71e03b508745054efcadb8a943a6c6e959c03c
Aviral Janveja <aviraljanveja@gmail.com>
2026-05-11 17:10:04 +0530

    Python code ch7 class_variables.py update

--- Code/07 - OOP/class_variables.py
+++ Code/07 - OOP/class_variables.py
@@ -2,27 +2,27 @@
 
 
 class Dog:
-    species = "Canine"  # Class variable, shared by all objects
+    species = "pet dogs"  # Class variable, shared by all objects
+    pet_num = 0  # Class variable to keep track of the number of dog objects created
 
     def __init__(self, name):
         self.name = name  # Object variable, unique to each object
+        Dog.pet_num += 1  # Increment the class variable pet_num each time a new dog object is created
 
 
-# Creating two objects
+# Creating Dog objects
 dog1 = Dog("Misty")
 dog2 = Dog("Leo")
+dog3 = Dog("Junior")
+dog4 = Dog("Brownie")
+
 
 # Accessing object variables
 print(dog1.name)  # Output = Misty
 print(dog2.name)  # Output = Leo
+print(dog3.name)  # Output = Junior
+print(dog4.name)  # Output = Brownie
 
-# Accessing class variable
-print(dog1.species)  # Output = Canine
-print(dog2.species)  # Output = Canine
 
-# Changing class variable
-Dog.species = "Wolf"
-
-# Now all objects reflect this change
-print(dog1.species)  # Output = Wolf
-print(dog2.species)  # Output = Wolf
+# Accessing class variables
+print(f"I have had {Dog.pet_num} {Dog.species} in life till now.")  # Output = I have had 4 pet dogs in life till now.
