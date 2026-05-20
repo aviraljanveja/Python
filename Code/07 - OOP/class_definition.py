c17d04cdc48ec3a71071a2417a16fb1ddb587794
Aviral Janveja <aviraljanveja@gmail.com>
2026-05-20 19:34:51 +0530

    Python code ch7 update 20 may 2026

--- Code/07 - OOP/class_definition.py
+++ Code/07 - OOP/class_definition.py
@@ -1,5 +1,5 @@
-# Class : a class in python is a blueprint for creating objects. It defines a set of attributes(variables) and methods(functions) that the created objects will have.
-# For example, the inbuilt list class in Python.
+# Class : a class in python is a blueprint for creating objects. It defines the data attributes(variables) 
+# and methods(functions) that the created objects will have.For example, the inbuilt list class in Python.
 
 # Object : an object is an instance of a class. It is a specific realization of the class with its own unique data.
 # For example, when we create a specific list object like list1 = [1, 2, 3]
@@ -8,7 +8,7 @@
 # Defining our own class.
 class Coordinate:
 
-    def __init__(self, x, y):  # Special method for initializing class objects, also called constructor.
+    def __init__(self, x, y):  # Special method for initializing class objects, also called the constructor.
         self.x = x  # self refers to the current object.
         self.y = y  # x and y are the coordinate values passed-in while creating the current object.
 
@@ -22,20 +22,20 @@ class Coordinate:
 
 
 # Creating objects of type Coordinate
-c = Coordinate(3, 4)
 o = Coordinate(0, 0)
+c = Coordinate(3, 4)
 
 
 # Accessing data-attributes of the object using dot-notation
-print(c.x)  # 3
 print(o.x)  # 0
+print(c.x)  # 3
 
 
 # Calling the distance method on Coordinate objects
 # As we have done for lists previously, for example list1.extend(list2)
-print(c.distance(o))  # 5.0
+print(o.distance(c))  # 5.0
 
 
 # Printing out the string representation of Coordinate objects as per the __str__ method defined above
-print(c)  # (3, 4)
 print(o)  # (0, 0)
+print(c)  # (3, 4)
